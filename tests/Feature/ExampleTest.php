1cbf2dc4af1678dbb6dce0d998d83f35f9050a70
sanjeev <sanjeev@mallow-tech.com>
2020-12-11 12:28:07 +0530

    commited test file

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,9 +4,13 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use App\Models\User;
+use App\Models\Post;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * A basic test example.
      *
@@ -17,5 +21,54 @@ class ExampleTest extends TestCase
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function user_can_visit_page_and_see_the_post()
+    {
+        $users = User::factory()->make();
+        $response = $this->actingAs($users)->get('posts');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function user_can_visit_post_and_create_the_post()
+    {
+        $users = User::factory()->make();
+        $this->actingAs($users);
+        $posts = Post::factory()->make(['user_id' => $users->id]);
+        $this->post('posts', $posts->toArray());
+        $response = $this->get('posts/create');
+        $response->assertOk();
+    }
+
+    /**
+     * @test
+     */
+    public function user_can_edit_and_update_the_post()
+    {
+        $users = User::factory()->make();
+        $this->actingAs($users);
+        $posts = post::factory()->create();
+        $this->get(' posts/'.$posts->id.'/edit ');
+        $posts = Post::factory()->make(['id' => $posts->id]);
+        $this->put('posts/' . $posts->id, $posts->toArray());
+        $this->assertDatabaseHas('posts', ['title' => $posts->title, 'id' => $posts->id, 'description' => $posts->description, 'posted_by' => $posts->posted_by]);
+    }
+
+    /**
+     * @test
+     */
+    public function user_can_delete_the_post()
+    {
+        $users = User::factory()->make();
+        $this->actingAs($users);
+        $posts = Post::factory()->create();
+        $this->delete('posts/'.$posts->id);
+        $this->assertDeleted($posts);
     }
 }
