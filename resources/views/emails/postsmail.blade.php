c2ea0632549c3f1d0bd7a38b85a891715c8bc3ca
sanjeev <sanjeev@mallow-tech.com>
2021-01-20 13:42:01 +0530

    pr

--- resources/views/emails/postsmail.blade.php
+++ resources/views/emails/postsmail.blade.php
@@ -4,12 +4,12 @@
     The body of your message.
     <div>
         @foreach($posts as $post)
-            <label>{{$post->title}}</label>
+            <label>{{ $post->title }}</label>
             <hr>
-            <label>{{$post->description}}</label>
+            <label>{{ $post->description }}</label>
             <hr>
         @endforeach
-        <label style="background-color: #0b2e13;">{{$message}}</label>
+        <label style="background-color: #0b2e13;">{{ $message }}</label>
     </div>
 
     @component('mail::button', ['url' => ''])
